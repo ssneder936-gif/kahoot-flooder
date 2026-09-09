965144f516c2c9aa035a850d97a62e8f7c4abed5
ssneder936-gif <ssneder936@gmail.com>
2026-09-09 16:26:03 -0400

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
 
 let isFlooding = false;
 let activeBots = [];
